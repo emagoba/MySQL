d6c35d5d61c52c109dd574666c64f4d426c1dc69
emagoba <137904392+emagoba@users.noreply.github.com>
2024-09-24 22:52:37 +0300

    Add files via upload

---
+++ Group By & Order By.sql
@@ -0,0 +1,14 @@
+select gender, avg(age), max(age), min(age), count(age)
+from employee_demographics
+group by gender
+;
+
+select occupation, salary
+from employee_salary
+group by occupation, salary
+;
+
+select *
+from employee_demographics
+order by gender, age asc
+;
---
+++ Select Statement.sql
@@ -0,0 +1,11 @@
+SELECT *
+FROM parks_and_recreation.employee_demographics;
+
+	SELECT first_name, 
+	last_name,
+	age,
+	age + 10
+	FROM parks_and_recreation.employee_demographics;
+    
+SELECT distinct gender
+FROM parks_and_recreation.employee_demographics;
